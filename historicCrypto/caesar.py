1debf20a0e75100976f8545321a419cf6e98c5e6
annettetrann <annettetran5@gmail.com>
2024-05-07 14:20:15 -0700

    finished caesar encryption

--- historicCrypto/caesar.py
+++ historicCrypto/caesar.py
@@ -1,33 +1,36 @@
 import math
 import string
 
-caesarDict = {}
 
-
-def caesarDecrypt(text):
-    text = string.ascii_lowercase
+def caesarDecode(text, outFile):
+    shifted = {}
+    f = open(outFile, "a")
     for i in range(26):
-        caesarDict[i] = ""
-        for j in text:
-            if j.isalpha():
-                if j.islower():
-                    caesarDict[i] += chr((ord(j) - i - 97) % 26 + 97)
-                elif j.isupper():
-                    caesarDict[i] += chr((ord(j) - i - 65) % 26 + 65)
+        shifted[i] = ""
+        for letter in text:
+            if letter.isalpha():
+                if letter.islower():
+                    shifted[i] += chr((ord(letter) - i - 97) % 26 + 97)
+                elif letter.isupper():
+                    shifted[i] += chr((ord(letter) - i - 65) % 26 + 65)
             else:
-                caesarDict[i] += j
-    return
+                shifted[i] += letter
+        f.write("Shifted " + str(i) + " | " + shifted[i])
+    f.close()
+    return 0
 
 
 
 
 def __main__():
     file = input("Enter the file name to decrypt by caesar : ")
+    outFile = input("Enter the file name to write the decrypted text : ")
     filePath = "encrypted 4/" + file
     fileToDecrypt = open(filePath, "r")
     text = fileToDecrypt.read()
-    # caesarDecrypt(text)
+    caesarDecode(text, outFile)
     print(text)
+    fileToDecrypt.close()
 
 
 if __name__ == "__main__":
